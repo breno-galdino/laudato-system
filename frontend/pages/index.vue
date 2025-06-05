7099e997fd5e96c25b26d84d40abe0a7c1b8df50
Breno Galdino <brenogaldino21@gmail.com>
2025-06-05 12:38:16 -0300

    ♻️ refact: backend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-    <div>
-        <v-btn @click="refresh">Refresh</v-btn>
-        {{ status }}{{ data ?? "WAITANDO" }}
-    </div>
+    <v-card height="100vh">
+        <n-color-picker />
+    </v-card>
 </template>
 <script setup>
-    const { data, status, error, refresh } = await useApi('/', {cache: 'no-cache'})
+    const { data, status, error, refresh } = await useApi('/', { cache: 'no-cache' })
 </script>
